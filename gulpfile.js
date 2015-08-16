07cc1774d4d7b978ad1e7fb0275ff33d5de2fed9
phamann <patrickhamann@gmail.com>
2015-08-16 23:28:25 +0100

    Add web-worker demo

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 gulp.task('server', function() {
-    gulp.src('./src/circuit-breakers')
+    gulp.src('./src/web-worker')
         .pipe(webserver({
             livereload: true,
             directoryListing: false,
